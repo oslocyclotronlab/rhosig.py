7ea2ac27ddc7352227ba9d590e6d0d4811eaa648
fzeiser <fzeiser@physnet.uni-hamburg.de>
2018-08-10 13:47:38 +0200

    Speed up decomposition using Cython
    Speedup the loops in decomposition by using cython. With my current settings, this saves about 1/2 of the runtime, for larger matrixes even more.
    More cpu time optimization can be achieved by playing the the minimization tolleraces.

--- rhosig.pyx
+++ rhosig.pyx
@@ -1,53 +1,68 @@
-from __future__ import division
-import numpy as np 
-import matplotlib.pyplot as plt 
-from scipy.optimize import minimize
-
+# cython: profile=True
 # Script to decompose the frist generations matrix P 
 # into the NLD $\rho$ and transmission coefficient $T$
 # (or $\gamma$-ray strength function $gsf$) respectivly
 
-def div0( a, b ):
+# to compile, run following:
+# cython rhosig.py
+# gcc -shared -pthread -fPIC -fwrapv -O2 -Wall -fno-strict-aliasing -I/usr/include/python2.7 -o rhosig.so rhosig.c
+
+from __future__ import division
+import numpy as np 
+import matplotlib.pyplot as plt 
+from scipy.optimize import minimize
+cimport cython
+cimport numpy as np
+
+def div0(np.ndarray a, np.ndarray b ):
   """ division function designed to ignore / 0, i.e. div0( [-1, 0, 1], 0 ) -> [0, 0, 0] """
+  cdef np.ndarray c
   with np.errstate(divide='ignore', invalid='ignore'):
     c = np.true_divide( a, b )
     c[ ~ np.isfinite( c )] = 0  # -inf inf NaN
   return c
 
-def PfromRhoT(rho, T, type="transCoeff"):
+@cython.boundscheck(False) # turn off bounds-checking for entire function
+@cython.wraparound(False)  # turn off negative index wrapping for entire function
+def PfromRhoT(np.ndarray rho, np.ndarray T, type="transCoeff"):
   # generate a first gernation matrix P from
   # given input:      rho and T  /or/ rho and gsf 
   # chosen by type= "transCoeff" /or/   "gsfL1"
-  Nbins = len(rho) #-- TODO: update for different #bins for Ex and Eg
-  P = np.zeros((Nbins,Nbins))
+  cdef int Nbins = len(rho) #-- TODO: update for different #bins for Ex and Eg
+  cdef int i_Ex, i_Eg, i_Ediff
+  cdef double Eg
+  global Emid
+  cdef np.ndarray P = np.zeros((Nbins,Nbins))
   for i_Ex in range(Nbins):
-    for i_Eg in range(i_Ex+1):
-      i_Ediff = i_Ex - i_Eg
-      if i_Ediff>=0: # no gamma's with higher energy then the excitation energy
-        P[i_Ex,i_Eg] = rho[i_Ediff] * T[i_Eg]
-        if type=="gsfL1": # if input T was a gsf, not transmission coeff: * E^(2L+1)
-          Eg = Emid[i_Eg]
-          P[i_Ex,i_Eg] *= pow(Eg,3.)
+      for i_Eg in range(i_Ex+1):
+        i_Ediff = i_Ex - i_Eg
+        if i_Ediff>=0: # no gamma's with higher energy then the excitation energy
+          P[i_Ex,i_Eg] = rho[i_Ediff] * T[i_Eg]
+          if type=="gsfL1": # if input T was a gsf, not transmission coeff: * E^(2L+1)
+            Eg = Emid[i_Eg]
+
+            P[i_Ex,i_Eg] *= np.power(Eg,3.)
+
   return P
 
-def chi2(rho, T, Pexp):
-    Pfit = PfromRhoT(rho, T)
+def chi2(np.ndarray rho, np.ndarray T, np.ndarray Pexp):
+    cdef np.ndarray Pfit = PfromRhoT(rho, T)
     # chi^2 = (data - fit)^2 / unc.^2, where unc.^2 = #cnt for Poisson dist.
-    chi2 = np.sum( div0(np.power((Pexp - Pfit), 2.),Pexp))
+    cdef float chi2 = np.sum( div0(np.power((Pexp - Pfit), 2.),Pexp))
     return chi2
 
-def rhoTfrom1D(x1D):
+def rhoTfrom1D(np.ndarray x1D):
   # split 1D array to who equal length subarrays
-  halflength = int(len(x1D)/2)
-  rho = x1D[:halflength]
-  T = x1D[halflength:]
+  cdef int halflength = int(len(x1D)/2)
+  cdef np.ndarray rho = x1D[:halflength]
+  cdef np.ndarray T = x1D[halflength:]
   return rho, T
 
 def objfun1D(x, *args):
   # 1D version of the chi2 function (needed for minimize function)
   # so x has one dimension only, but may be nested to contain rho and T
   Pexp = np.array(args)
-  rho, T= rhoTfrom1D(x)
+  rho, T = rhoTfrom1D(x)
   return chi2(rho, T, Pexp)
 
 def decompose_matrix(P_in, Emid, fill_value=0):
@@ -88,6 +103,7 @@ def decompose_matrix(P_in, Emid, fill_value=0):
   # minimization
   res = minimize(objfun1D, x0=p0, args=P_in, method="Powell",
     options={'disp': True})
+  # further optimization: eg through higher tolderaced xtol and ftol
   # different other methods tried:
   # res = minimize(objfun1D, x0=p0, args=P_in,
   #   options={'disp': True})
