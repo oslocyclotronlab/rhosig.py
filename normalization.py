Correct Chi^2 in normalization.py
Commented out the devision by number of points in the normalization

normalization.py
@@ -97,7 +97,7 @@ class NormNLD:
 
         if model=="CT":
             pars_req = {"T", "Eshift"}
-            if pars.has_key("nld_Sn") and pars.has_key("Eshift")==False:
+            if ("nld_Sn" in pars) and ("Eshift" in pars == False):
                 pars["Eshift"] = self.EshiftFromT(pars["T"], pars["nld_Sn"])
             pars["Earr"] = Earr
             values = ut.call_model(self.CT,pars,pars_req)
@@ -196,6 +196,10 @@ class NormNLD:
         self.pext["T"] = T
         self.pext["Eshift"] = self.EshiftFromT(T, nld_Sn)
 
+        import multinest as ml
+
+        ml.run_nld_2regions(parameters = ["A", "alpha", "T"], args=(nldModel, nld_Sn, data_low, data_high, levels_smoothed))
+        print(res)
         return res.x
 
     @staticmethod
@@ -258,17 +262,20 @@ class NormNLD:
         chi2 = (data[:,1] - levels_smoothed)**2.
         if data.shape[1] == 3: # weight with uncertainty, if existent
             chi2 /= data[:,2]**2
-        chi2_low = np.sum(chi2)/n_low
+        chi2_low = np.sum(chi2)#/n_low
 
         data = NormNLD.normalize(data_high, A, alpha)
         n_high = len(data)
         Eshift = NormNLD.EshiftFromT(T, nld_Sn)
         chi2 = (data[:,1] - nldModel(data[:,0], T, Eshift))** 2.
         if data.shape[1] == 3: # weight with uncertainty, if existent
-            chi2 /= data[:,2]**2
-        chi2_high = np.sum(chi2)/n_high
+            chi2 /= (data[:,2])**2
+        chi2_high = np.sum(chi2)#/n_high
 
-        chi2 = (chi2_low + chi2_high)*(n_high+n_low)
+        chi2 = (chi2_low + chi2_high)#*(n_high+n_low)
+
+        # if abs(T-0.41)>0.01:
+        #     chi2 += 10e5
         return chi2
 
 
@@ -461,7 +468,7 @@ def transformGSF(Emid_Eg, Emid_nld, rho_in, gsf_in,
   gsf_ext_low_norm[:,1] *= b_norm
   gsf_ext_high_norm[:,1] *= b_norm
 
-  print("alpha_norm: {0}".format(alpha_norm))
+  # print("alpha_norm: {0}".format(alpha_norm))
   print("b_norm: {0}".format(b_norm))
 
   return gsf_norm, gsf_ext_low_norm, gsf_ext_high_norm, b_norm
@@ -577,9 +584,9 @@ def normalizeGSF(Emid_Eg, Emid_nld, rho_in, gsf_in,
           ext_d_slider_ax  = fig.add_axes([0.25, 0.20, 0.65, 0.03], facecolor=axis_color)
 
           sext_a = Slider(ext_a_slider_ax, 'a', 0., 2., valinit=ext_a)
-          sext_b = Slider(ext_b_slider_ax, 'b', 0, 5, valinit=ext_b)
+          sext_b = Slider(ext_b_slider_ax, 'b', -3, 5, valinit=ext_b)
           sext_c = Slider(ext_c_slider_ax, 'c', 0, 2., valinit=ext_c)
-          sext_d = Slider(ext_d_slider_ax, 'd', 0, 5, valinit=ext_d)
+          sext_d = Slider(ext_d_slider_ax, 'd', -3, 5, valinit=ext_d)
 
           def slider_update(val):
               # nonlocal gsf, gsf_ext_low, gsf_ext_high, b_norm

standard_plots.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import rhosig as rsg
 
-def rsg_plots(rho_fit, T_fit, P_in, Emid_Eg, Emid_nld, Emid_Ex, Exmin, Egmin,nld_ext=None, rho_true=None, discretes=None, **kwargs):
+def rsg_plots(rho_fit, T_fit, P_in, Emid_Eg, Emid_nld, Emid_Ex, Exmin, Emax, Egmin,nld_ext=None, rho_true=None, discretes=None, **kwargs):
     Nbins_Ex, Nbins_Eg= np.shape(P_in) # after
     bin_width = Emid_Eg[1]- Emid_Eg[0]
     Eup_max = Exmin + Nbins_Ex * bin_width # upper bound of last bin
@@ -64,7 +64,36 @@ def rsg_plots(rho_fit, T_fit, P_in, Emid_Eg, Emid_nld, Emid_Ex, Exmin, Egmin,nld
     ax.set_xlabel(r"$E_x \, \mathrm{(MeV)}$")
     ax.set_ylabel(r'$\rho \, \mathrm{(MeV)}$')
 
+    # "does it work" plots
+    # = comparison of slices of the 1st gen matrixes
+    Nx, Ny = 3, 2
+    Ntot = Nx * Ny
+    Exs = np.linspace(Exmin,Emax,num=Ntot)
+
+    f_mat, ax_mat = plt.subplots(Ny,Nx)
+    for i in range(Ntot):
+        Ex_plt = Exs[i] # Ex for this plot
+        iEx = np.abs(Emid_Ex-Ex_plt).argmin()
+        i_plt, j_plt = map_iterator_to_grid(i, Nx)
+        ax = ax_mat[i_plt,j_plt]
+        ax.plot(P_in[iEx,:],"k-", label="input")
+        ax.plot(P_fit[iEx,:],"b-", label="fit")
+        ax.title.set_text('Ex = {:.1f}'.format(Ex_plt))
+
+        ax.set_xlabel(r"$E_g \, \mathrm{(MeV)}$")
+        ax.set_ylabel('Probability')
+
+    ax_mat[0,0].legend()
+
+    plt.tight_layout()
     plt.show()
+
+def map_iterator_to_grid(counter, Nx):
+    # Returns i, j coordinate pairs to map a single iterator onto a 2D grid for subplots.
+    # Counts along each row from left to right, then increments row number
+    i = counter // Nx
+    j = counter % Nx
+    return i, j
 
 def normalized_plots(rho_fit, gsf_fit, gsf_ext_low, gsf_ext_high, Emid_Eg, Emid_nld, rho_true=None, rho_true_binwidth=None, gsf_true=None):
     # New Figure: compare input and output NLD and gsf